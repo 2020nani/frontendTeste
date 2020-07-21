6c73a2535fdb63125218aeaf2d0daba99b0589a0
hernani <megahack9@outlook.com>
2020-07-21 00:36:38 -0300

    rotas configuradas,pagina header e home

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import Header from './components/Header';
+import Routes from './routes';
+
+import history from './services/history';
+
+
+export default function App() {
+  return (
+   
+      <Router history={history}>
+        <Header />
+        <Routes />
+      </Router>
+    
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+
+export default function App() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+    </Switch>
+  );
+}
