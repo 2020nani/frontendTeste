69bccc5b6b2b3b93ef5f72b9e2654fc738108d09
hernani <megahack9@outlook.com>
2020-07-23 22:49:07 -0300

    acessando api

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {useState,useEffect}from 'react';
+import {Form, Input} from '@rocketseat/unform'
+import api from '../../services/api';
+import {Container} from '../Header/styles';
+import * as Yup from 'yup';
 
-import {Container} from '../Header/styles'
+const schema = Yup.object().shape({
+  firstname: Yup.string()
+  .required('Firstname e obrigatorio'),
+  lastname: Yup.string()
+  .required('Lastname e obrigatorio'),
+  participation: Yup.number()
+  .integer('Apenas numeros')
+  .required('participation e obrigatorio')
+})
 export default function Header() {
+  
+  function handleSubmit(data) {
+    console.log(data)
+  }
   
   return (
     <Container>
       
-     <form action="">
-     <input type="text" placeholder="firstname"/>
-     <input type="text" placeholder="lastname"/>
-     <input type="text" placeholder="participation"/>
+     <Form schema={schema} onSubmit={handleSubmit}>
+     <Input name="firstname" type="text" placeholder="firstname"/>
+     <Input name="lastname" type="text" placeholder="lastname"/>
+     <Input name="participation" type="text" placeholder="participation"/>
      <button type="button">Send</button>
      
-     </form>
+     </Form>
      
     </Container>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,63 +1,74 @@
-import React,{useState}  from 'react';
-import {Chart} from 'react-google-charts'
-import {Container,Grafico,Tabela,Session} from '../Home/styles'
+import React, { useState, useEffect } from 'react';
+import { Chart } from 'react-google-charts'
+import { Container, Grafico, Tabela, Session } from '../Home/styles'
+import api from '../../services/api'
 
 export default function Home() {
   const [options, setOptions] = useState({
     title: 'Grafico de participacao',
     is3D: true
   })
-  const [data,setData] = useState([
+  const [data, setData] = useState([
     ['nome', 'participacao'],
     ['hernani', 100],
     ['junior', 90],
     ['vania', 110],
-    ['mauro',90]
+    ['mauro', 90]
   ])
+  const [dados, setDados] = useState([])
+
+  useEffect(() => {
+    async function loadDados() {
+
+      const response = await api.get('dados')
+      response.data.map(elem=>(
+        data.push([elem.firstname,elem.participation])
+      ))
+      setDados(response.data)
+      
+      
+    }
+    loadDados()
+  }, [])
+
   return (
-   <Container>
-     <h1>DATA</h1>
-     <p>lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
-   <Session> 
-    <Tabela>
-     <table>
-       <tr>
-         <th></th>
-         <th>FirstName</th>
-         <th>LastName</th>
-         <th>Participation</th>
-       </tr>
-       <tr>
-         <td>1</td>
-         <td>Hernani</td>
-         <td>Almeida</td>
-         <td>20%</td>
-       </tr>
-       <tr>
-         <td>1</td>
-         <td>Hernani</td>
-         <td>Almeida</td>
-         <td>20%</td>
-       </tr>
-       <tr>
-         <td>1</td>
-         <td>Hernani</td>
-         <td>Almeida</td>
-         <td>20%</td>
-       </tr>
-     </table>
-     </Tabela>
-     <Grafico>
-      <Chart
-      width={'100%'}
-      chartType="PieChart"
-      data={data}
-      options={options}
-      />
-     </Grafico>
-     </Session>
-   </Container>
-     
+    <Container>
+      <h1>DATA</h1>
+      <p>lorem ipsum dolor sit amet consectetuer adipiscing elit</p>
+      <Session>
+      
+        <Tabela>
+          <table>
+            <tr>
+              <th></th>
+              <th>FirstName</th>
+              <th>LastName</th>
+              <th>Participation</th>
+            </tr>
+            {dados.map(data =>(
+              <tr>
+              <td>{data.id}</td>
+            <td>{data.firstname}</td>
+            <td>{data.lastname}</td>
+            <td>{data.participation}</td>
+            </tr>
+            ))}
+            <button type="button">Atualizar</button>
+            <button type="button">Deletar</button>
+          </table>
+        </Tabela>
+        <Grafico>
+          <Chart
+            width={'100%'}
+            chartType="PieChart"
+            data={data}
+            options={options}
+          />
+        </Grafico>
+        
+      </Session>
+    </Container>
+
   );
 }
 
